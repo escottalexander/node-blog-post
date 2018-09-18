f2e5cea37535b911bfa1b6c22bac6b5a5d4f5a1e
Elliott Alexander <escottalexander@gmail.com>
2018-09-18 18:41:07 -0400

    added and tested all methods for this application

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models');
 
@@ -9,7 +12,7 @@ router.get('/', (req, res) => {
   
   
   
-  router.post('/', (req, res) => {
+  router.post('/', jsonParser, (req, res) => {
     const requiredFields = ['title', 'content', 'author'];
     for (let i=0; i<requiredFields.length; i++) {
       const field = requiredFields[i];
@@ -24,19 +27,13 @@ router.get('/', (req, res) => {
   });
   
   
-  // when DELETE request comes in with an id in path,
-  // try to delete that item from ShoppingList.
   router.delete('/:id', (req, res) => {
     BlogPosts.delete(req.params.id);
-    console.log(`Deleted blog entry \`${req.params.ID}\``);
+    console.log(`Deleted blog entry \`${req.params.id}\``);
     res.status(204).end();
   });
   
-  // when PUT request comes in with updated item, ensure has
-  // required fields. also ensure that item id in url path, and
-  // item id in updated item object match. if problems with any
-  // of that, log error and send back status code 400. otherwise
-  // call `ShoppingList.update` with updated item.
+
   router.put('/:id', jsonParser, (req, res) => {
     const requiredFields = ['title', 'content', 'author', 'id'];
     for (let i=0; i<requiredFields.length; i++) {
@@ -55,12 +52,11 @@ router.get('/', (req, res) => {
       return res.status(400).send(message);
     }
     console.log(`Updating blog entry \`${req.params.id}\``);
-    const updatedItem = ShoppingList.update({
+    const updatedItem = BlogPosts.update({
       id: req.params.id,
       title: req.body.title,
       content: req.body.content,
-      author: req.body.author,
-      publishDate: req.body.publishDate
+      author: req.body.author
     });
     res.status(204).end();
   })
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-
-const blogPostRouter = rrequire('./blogPostRouter');
+const {BlogPosts} = require('./models');
+const blogPostRouter = require('./blogPostRouter');
 
 const app = express();
 
@@ -11,3 +11,7 @@ BlogPosts.create("The Happy Hare", "Once upon a time, there lived a hare named..
 BlogPosts.create("The Rabid Racoon", "Once upon a time, there lived a racoon named...", "Elliott Alexander");
 
 app.use('/blog-posts', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+  });
